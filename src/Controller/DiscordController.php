2f62c4413706ea06c5fec9ef244c1c9123af7883
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-07-19 15:58:12 +0200

    updt

---
+++ src/Controller/DiscordController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route("/auth/discord", name: "auth_discord_")]
+final class DiscordController
+{
+    #[Route("/login", name: "login")]
+    public function login(Request $request, ClientRegistry $clientRegistry)
+    {
+    }
+
+    #[Route("/start", name: "start")]
+    public function start(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        return $clientRegistry->getClient("discord")->redirect(["identify"]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $discordId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDiscordId(): ?string
+    {
+        return $this->discordId;
+    }
+
+    public function setDiscordId(string $discordId): static
+    {
+        $this->discordId = $discordId;
+
+        return $this;
+    }
+    
+    public function getRoles(): array
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        // TODO: Implement getUserIdentifier() method.
+    }
+}
